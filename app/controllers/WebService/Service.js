bb8423570e871f685f03fe62139bf0405fb29920
Njara <njaraliantsoa@hotmail.fr>
2015-03-03 17:46:10 +0100

    webservice

--- app/controllers/WebService/Service.js
+++ app/controllers/WebService/Service.js
@@ -4,11 +4,33 @@
 var mongoose = require('mongoose');
 //var photoDao=require('/PhotoDao');
 var  photosDao=require('../../dao/PhotosDao');
+var userDao=require('../../dao/UsersDao');
 module.exports = function(app) {
     app.get('/ListePhotoPublier', function (req, res){
             photosDao.getListImageNow(function(cb){
-                   // console.dir(cb);
+                   console.dir(cb);
                    res.json(cb);
             });
+    });
+    app.post('/LoginWS/Membre', function (req, res) {
+        console.dir("ezezeze");
+        var data={
+            email:req.body.email,
+            mdp:req.body.password
+        }
+        userDao.testLoginMembre(data,function(cb){
+
+            if(!cb)
+                console.dir("Email ou mot de passe incorrect");
+            else {
+                console.dir(cb);
+                req.session.user=cb;
+                delete  req.session.user.mdp;
+                req.session.user=cb;
+                res.json(req.session.user);
+            }
+
+        })
+
     });
 }
